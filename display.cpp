adc0aeab0c8953645665a69158d91dac3b4520fd
Bill Siever <bsiever@gmail.com>
2022-09-05 11:10:47 -0500

    Updates for updated CODAL.

--- display.cpp
+++ display.cpp
@@ -12,9 +12,7 @@
 */
 
 #include "pxt.h"
-#include "MicroBitI2C.h"
 #include "MicroBit.h"
-
 
 using namespace pxt;
 
@@ -22,82 +20,12 @@ namespace display {
     
    //%
    void _rotateTo(int direction) {
-    static int lastDirection = 0;
-    if(direction<0 || direction>3) direction = 0;
-    // If not changed, return
-    if(lastDirection == direction) {
-        return;
-    }
-    lastDirection = direction;
-
     /*
-     MATRIX_DISPLAY_ROTATION_0,
-        MATRIX_DISPLAY_ROTATION_90,
-        MATRIX_DISPLAY_ROTATION_180,
-        MATRIX_DISPLAY_ROTATION_270
-        */
-#if MICROBIT_CODAL
-static  MatrixPoint normal[5*5] =
-{
-    {0,0},{0,1},{0,2},{0,3},{0,4},
-    {1,0},{1,1},{1,2},{1,3},{1,4},
-    {2,0},{2,1},{2,2},{2,3},{2,4},
-    {3,0},{3,1},{3,2},{3,3},{3,4},
-    {4,0},{4,1},{4,2},{4,3},{4,4}
-};
-
-static  MatrixPoint logoright[5*5] =
-{
-    {4,0},{3,0},{2,0},{1,0},{0,0},
-    {4,1},{3,1},{2,1},{1,1},{0,1},
-    {4,2},{3,2},{2,2},{1,2},{0,2},
-    {4,3},{3,3},{2,3},{1,3},{0,3},
-    {4,4},{3,4},{2,4},{1,4},{0,4}
-};
-
-static  MatrixPoint logoleft[5*5] =
-{
-    {0,4},{1,4},{2,4},{3,4},{4,4},
-    {0,3},{1,3},{2,3},{3,3},{4,3},
-    {0,2},{1,2},{2,2},{3,2},{4,2},
-    {0,1},{1,1},{2,1},{3,1},{4,1},
-    {0,0},{1,0},{2,0},{3,0},{4,0}
-};
-
-static  MatrixPoint upsidedown[5*5] =
-{
-    {4,4},{4,3},{4,2},{4,1},{4,0},
-    {3,4},{3,3},{3,2},{3,1},{3,0},
-    {2,4},{2,3},{2,2},{2,1},{2,0},
-    {1,4},{1,3},{1,2},{1,1},{1,0},
-    {0,4},{0,3},{0,2},{0,1},{0,0}
-};
-
-    // Get pins
-    static NRF52Pin* ledRowPins[5] = {&uBit.io.row1, &uBit.io.row2, &uBit.io.row3, &uBit.io.row4, &uBit.io.row5};
-    static NRF52Pin* ledColPins[5] = {&uBit.io.col1, &uBit.io.col2, &uBit.io.col3, &uBit.io.col4, &uBit.io.col5};
-    static MatrixMap ledMatrixMap[4] = 
-    {
-        {5, 5, 5, 5, (Pin**)ledRowPins, (Pin**)ledColPins, normal},
-        {5, 5, 5, 5, (Pin**)ledRowPins, (Pin**)ledColPins, logoright},
-        {5, 5, 5, 5, (Pin**)ledRowPins, (Pin**)ledColPins, upsidedown},
-        {5, 5, 5, 5, (Pin**)ledRowPins, (Pin**)ledColPins, logoleft},
-
-    };
-    // Display old instance
-    // Stop animations (to avoid retained objects)
-    uBit.display.stopAnimation();
-    static MicroBitDisplay displays[4] = {
-        uBit.display,
-        MicroBitDisplay(ledMatrixMap[1]),
-        MicroBitDisplay(ledMatrixMap[2]),
-        MicroBitDisplay(ledMatrixMap[3])
-    };
-    uBit.display.disable();
-    memcpy((void*)&uBit.display, (void*)&displays[direction], sizeof(MicroBitDisplay));
-    uBit.display.enable();
-#else
+    MATRIX_DISPLAY_ROTATION_0,
+    MATRIX_DISPLAY_ROTATION_90,
+    MATRIX_DISPLAY_ROTATION_180,
+    MATRIX_DISPLAY_ROTATION_270
+    */
        uBit.display.rotateTo((DisplayRotation)direction);
-#endif   
     }
 }
